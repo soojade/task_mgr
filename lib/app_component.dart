be7de51b1c267b4cba49c0ab0f16a8db713a83a4
soojade <soojade@sina.com>
2018-05-02 16:48:18 +0800

    删除 todo_list

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,5 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/todo_list_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -9,7 +8,7 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
